c1d287a225bc40e97b4db02ade7c66deb1a9601f
Mrosstro <ttosam01@gmail.com>
2017-05-12 13:15:06 +0800

    version 1.5.0

--- IceboxAndCookbook/SeeViewController.swift
+++ IceboxAndCookbook/SeeViewController.swift
@@ -14,16 +14,19 @@ class SeeViewController: UIViewController, UITableViewDataSource, UITableViewDel
     var testimage: String = "image小黃瓜"
     var testName: String = "涼拌小黃瓜"
     var testIngredients: String = "小黃瓜300克,細砂糖40克,鹽1.5小匙,白醋40毫升,辣椒1根,蒜泥或薑泥1匙,香油數滴"
-    var testDescription: String = "阿基師小黃瓜偷呷步快速入味法，臨時想吃的時候，完全不需等待、馬上入味^O^,鹽讓小黃瓜出水，所以也流失水分，反而使小黃瓜失去多汁的爽脆口感。,應該先使用糖，利用它的滲透壓讓小黃瓜快速入味。,糖和白醋都是小黃瓜的1/6，鹽少許即可。,但我不想吃太多糖，把糖和醋都減少10克也很入味喔。"
+    var testDescription: String = "Step 1\n小黃瓜洗淨切除頭尾、切小段後拍碎(先拍後切也OK)。先加糖用手抓一抓後再加鹽拌勻\n\nStep 2\n先加糖用手抓一抓後再加鹽拌勻加入薑泥或蒜泥(磨成泥才能迅速入味)、辣椒和白醋拌勻，盛盤後滴入幾滴香油在小黃瓜上面，這樣就完成了。\n\nStep 3\n加入薑泥或蒜泥(磨成泥才能迅速入味)、辣椒和白醋拌勻， 盛盤後滴入幾滴香油在小黃瓜上面， 這樣就完成了。好快喔！趕快試吃一個真有入味嗎??~~好脆喔、酸酸甜甜，的確有入味耶\n\nStep 4\n好快喔！趕快試吃一個真有入味嗎?? ~~好脆喔、酸酸甜甜，的確有入味耶"
     
 
     @IBOutlet weak var SImage: UIImageView!
+
+    @IBAction func ClickBack(_ sender: UIBarButtonItem) {
+        self.navigationController?.popViewController(animated: true)
+    }
     
     //▼初始化
     override func viewDidLoad() {
         super.viewDidLoad()
-//        tableView.estimatedRowHeight = 36
-//        tableView.rowHeight = UITableViewAutomaticDimension
+        
         
         SImage.image = UIImage(named: testimage)
     }
@@ -37,6 +40,8 @@ class SeeViewController: UIViewController, UITableViewDataSource, UITableViewDel
     
     //▼有幾組 row
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tableView.estimatedRowHeight = 36
+        tableView.rowHeight = UITableViewAutomaticDimension
         return 3
     }
     
@@ -60,7 +65,7 @@ class SeeViewController: UIViewController, UITableViewDataSource, UITableViewDel
             cell.SValue.text = testIngredients
             print("完成食材")
         case 2:
-            cell.SField.text = "描述"
+            cell.SField.text = "做法"
             cell.SValue.text = testDescription
         default:
             cell.SField.text = ""
